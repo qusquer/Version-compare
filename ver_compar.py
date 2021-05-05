512cf2644bb885c97618ba5bfb7c984155fd2d21
qusquer <daniilyurkondratev@yandex.ru>
2021-05-06 02:25:54 +0300

    учтен ввод посторонних типов данных

--- ver_compar.py
+++ ver_compar.py
@@ -1,4 +1,15 @@
 def ver_compare(ver1, ver2):
+    
+    if not(isinstance(ver1, str) and isinstance(ver2, str)): 
+        raise TypeError('ver must be str')
+
+    ver1 = ver1.split('.')
+    ver2 = ver2.split('.')
+    
+    for i in (ver1 + ver2):
+        if not(i.isdigit()):
+            raise ValueError("a string can't contain anything other than numbers and a dot")
+    
     ver1 = [int(x) for x in ver1]
     ver2 = [int(x) for x in ver2]
     
@@ -17,7 +28,5 @@ def ver_compare(ver1, ver2):
         elif ver1[i] < ver2[i]:
             return -1
     return 0
-            
-        
-    
-print(ver_compare((input().split('.')), (input().split('.'))))
+               
+print(ver_compare(input(), input()))
